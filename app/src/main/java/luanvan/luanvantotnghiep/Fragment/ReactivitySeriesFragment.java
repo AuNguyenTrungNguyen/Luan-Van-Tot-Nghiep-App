9452927cfdb5ebee27486b7da666c717d8aa3d8d
Admin <sangb1401179@gmail.com>
2018-09-04 16:19:25 +0700

    update dãy hoạt động

--- app/src/main/java/luanvan/luanvantotnghiep/Fragment/ReactivitySeriesFragment.java
+++ app/src/main/java/luanvan/luanvantotnghiep/Fragment/ReactivitySeriesFragment.java
@@ -7,11 +7,19 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Html;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,6 +34,12 @@ public class ReactivitySeriesFragment extends Fragment {
     private ChemistryHelper mChemistryHelper;
     private Context mContext;
     private ReactSeriesAdapter mAdapter;
+
+    private TextView mTvOxidizing;
+    private TextView mTvReducing;
+
+    private TextView mTvMemorize;
+    private TextView mTvMeaning;
 
     public ReactivitySeriesFragment() {
     }
@@ -52,12 +66,20 @@ public class ReactivitySeriesFragment extends Fragment {
 
         showReactivityList();
 
+        showMemorizeAndMeaning();
+
         return view;
     }
 
     private void init(View v) {
         mChemistryHelper = ChemistrySingle.getInstance(mContext);
         List<ReactSeries> mReactSeriesList = mChemistryHelper.getAllReactSeries();
+
+        mTvOxidizing = v.findViewById(R.id.tv_oxidizing_agent);
+        mTvReducing = v.findViewById(R.id.tv_reducing_agent);
+
+        mTvMemorize = v.findViewById(R.id.tv_memorize_react_series);
+        mTvMeaning = v.findViewById(R.id.tv_meaning_react_series);
 
         RecyclerView mRvReactSeries = v.findViewById(R.id.rv_react_series);
         mAdapter = new ReactSeriesAdapter(mContext, mReactSeriesList);
@@ -148,6 +170,33 @@ public class ReactivitySeriesFragment extends Fragment {
             for (ReactSeries item : reactSeriesList) {
                 mChemistryHelper.addReactSeries(item);
             }
+        }
+    }
+
+    private void showMemorizeAndMeaning(){
+        mTvOxidizing.setText(Html.fromHtml("Tính chất oxi hóa của ion kim loại tăng dần &rarr"));
+        mTvReducing.setText(Html.fromHtml("&larr Tính chất khử của kim loại tăng dần"));
+
+        mTvMemorize.setText("Lúc khác Ba Cần Nên Mang Áo Giáp Có Sắt2/Sắt" +
+                " Nên sang Phố Sắt3/Sắt Hỏi cửa Hàng Sắt3/Sắt2 Hiệu Á Phi Âu");
+
+        try {
+            InputStream fis = mContext.getAssets().open("reactivity_series.txt");
+            BufferedReader reader=new
+                    BufferedReader(new InputStreamReader(fis));
+            String data="";
+            StringBuilder builder=new StringBuilder();
+            while((data=reader.readLine())!=null)
+            {
+                builder.append(data);
+                builder.append("\n");
+            }
+            fis.close();
+            mTvMeaning.setText(Html.fromHtml(builder.toString()));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
