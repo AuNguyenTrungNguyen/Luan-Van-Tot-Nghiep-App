439d04d0ac4b67e2434c6330ca8dc8f8182e0500
Admin <sangb1401179@gmail.com>
2018-09-14 09:17:58 +0700

    set text color, choose click and edit text info detail element

--- app/src/main/java/luanvan/luanvantotnghiep/Activity/DetailElementActivity.java
+++ app/src/main/java/luanvan/luanvantotnghiep/Activity/DetailElementActivity.java
@@ -11,9 +11,13 @@ import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -45,7 +49,6 @@ public class DetailElementActivity extends AppCompatActivity implements View.OnC
     private LinearLayout mLnProton;
     private LinearLayout mLnNeutron;
 
-
     private ImageView mImgBackground;
     private ImageView mImgWiki;
 
@@ -59,7 +62,7 @@ public class DetailElementActivity extends AppCompatActivity implements View.OnC
     private TextView mTvGroup;
     private TextView mTvPeriodic;
     private TextView mTvElectronegativity;
-    private TextView mTvWikiPaulingScale;
+    //private TextView mTvWikiPaulingScale;
     private TextView mTvValence;
 
     private TextView mTvClass;
@@ -70,7 +73,7 @@ public class DetailElementActivity extends AppCompatActivity implements View.OnC
     private TextView mTvStatus;
     private TextView mTvColor;
     private TextView mTvIsotope;
-    private ImageView mImgWikiTableIsotope;
+    //private ImageView mImgWikiTableIsotope;
     private TextView mTvMeltingPoint;
     private TextView mTvBoilingPoint;
 
@@ -88,6 +91,19 @@ public class DetailElementActivity extends AppCompatActivity implements View.OnC
     private TextView mTvInfoElement;
 
     private String mStrCategory;
+
+    private ImageButton mIBInfoElectron;
+    private ImageButton mIBInfoProton;
+    private ImageButton mIBInfoNeutron;
+    private ImageButton mIBElementCategory;
+    private ImageButton mIBWeight;
+    private ImageButton mIBGroup;
+    private ImageButton mIBPeriodic;
+    private ImageButton mIBElectronegativity;
+    private ImageButton mIBClass;
+    private ImageButton mIBValence;
+    private ImageButton mIBConfiguration;
+    private ImageButton mIBIsotope;
 
 
     @Override
@@ -120,8 +136,8 @@ public class DetailElementActivity extends AppCompatActivity implements View.OnC
 
     private void addEvents() {
         mImgWiki.setOnClickListener(this);
-        mTvWikiPaulingScale.setOnClickListener(this);
-        mImgWikiTableIsotope.setOnClickListener(this);
+        //mTvWikiPaulingScale.setOnClickListener(this);
+        //mImgWikiTableIsotope.setOnClickListener(this);
         mElectronView.setOnClickListener(this);
 
         mLnElectron.setOnClickListener(this);
@@ -140,6 +156,19 @@ public class DetailElementActivity extends AppCompatActivity implements View.OnC
         mTvSimplified.setOnClickListener(this);
 
         mTvIsotope.setOnClickListener(this);
+
+        mIBInfoElectron.setOnClickListener(this);
+        mIBInfoProton.setOnClickListener(this);
+        mIBInfoNeutron.setOnClickListener(this);
+        mIBElementCategory.setOnClickListener(this);
+        mIBWeight.setOnClickListener(this);
+        mIBGroup.setOnClickListener(this);
+        mIBPeriodic.setOnClickListener(this);
+        mIBElectronegativity.setOnClickListener(this);
+        mIBValence.setOnClickListener(this);
+        mIBClass.setOnClickListener(this);
+        mIBConfiguration.setOnClickListener(this);
+        mIBIsotope.setOnClickListener(this);
     }
 
     private void init() {
@@ -163,7 +192,7 @@ public class DetailElementActivity extends AppCompatActivity implements View.OnC
         mTvGroup = findViewById(R.id.tv_group);
         mTvPeriodic = findViewById(R.id.tv_period);
         mTvElectronegativity = findViewById(R.id.tv_electronegativity);
-        mTvWikiPaulingScale = findViewById(R.id.tv_wiki_pauling_scale);
+        //mTvWikiPaulingScale = findViewById(R.id.tv_wiki_pauling_scale);
         mTvValence = findViewById(R.id.tv_valence);
 
         mTvClass = findViewById(R.id.tv_class);
@@ -174,13 +203,25 @@ public class DetailElementActivity extends AppCompatActivity implements View.OnC
         mTvStatus = findViewById(R.id.tv_status_chemical);
         mTvColor = findViewById(R.id.tv_color_chemical);
         mTvIsotope = findViewById(R.id.tv_isotope);
-        mImgWikiTableIsotope = findViewById(R.id.img_wiki_table_isotope);
+        //mImgWikiTableIsotope = findViewById(R.id.img_wiki_table_isotope);
         mTvMeltingPoint = findViewById(R.id.tv_melting_point);
         mTvBoilingPoint = findViewById(R.id.tv_boiling_point);
 
         mTvDiscoverer = findViewById(R.id.tv_discoverer);
         mTvYearDiscovery = findViewById(R.id.tv_year_discovery);
 
+        mIBInfoElectron = findViewById(R.id.ib_info_electron);
+        mIBInfoProton = findViewById(R.id.ib_info_proton);
+        mIBInfoNeutron = findViewById(R.id.ib_info_neutron);
+        mIBElementCategory = findViewById(R.id.ib_info_category);
+        mIBWeight = findViewById(R.id.ib_info_weight);
+        mIBGroup = findViewById(R.id.ib_info_group);
+        mIBPeriodic = findViewById(R.id.ib_info_period);
+        mIBElectronegativity = findViewById(R.id.ib_info_electronegativity);
+        mIBClass = findViewById(R.id.ib_info_class);
+        mIBValence = findViewById(R.id.ib_info_valence);
+        mIBConfiguration = findViewById(R.id.ib_info_configuration);
+        mIBIsotope = findViewById(R.id.ib_info_isotope);
     }
 
     private String getNameTypeById(int id) {
@@ -220,7 +261,7 @@ public class DetailElementActivity extends AppCompatActivity implements View.OnC
                 mTvValence.setText(Html.fromHtml("<font color='gray'><b>Hóa trị: </b></font><font color='black'>" + element.getValence() + "</font>"));
 
                 mTvClass.setText(Html.fromHtml("<font color='gray'>Phân lớp: </font><font color='black'>" + element.getClassElement() + "</font>"));
-                mTvSimplified.setText(Html.fromHtml("<font color='gray'>Electron rút rọn: </font><font color='black'>" + element.getSimplifiedConfiguration() + "</font>"));
+                mTvSimplified.setText(Html.fromHtml("<font color='black'>" + element.getSimplifiedConfiguration() + "</font>"));
 
                 mTvStatus.setText(Html.fromHtml("<font color='gray'>Trạng thái: </font><font color='black'>" + chemistry.getStatusChemistry() + "</font>"));
                 mTvColor.setText(Html.fromHtml("<font color='gray'>Màu sắc: </font><font color='black'>" + chemistry.getColorChemistry() + "</font>"));
@@ -297,17 +338,17 @@ public class DetailElementActivity extends AppCompatActivity implements View.OnC
                 startActivity(intent);
                 break;
 
-            case R.id.tv_wiki_pauling_scale:
-                uri = "https://en.wikipedia.org/wiki/Electronegativities_of_the_elements_(data_page)";
-                intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
-                startActivity(intent);
-                break;
-
-            case R.id.img_wiki_table_isotope:
-                uri = "https://en.wikipedia.org/wiki/Table_of_nuclides_(combined)";
-                intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
-                startActivity(intent);
-                break;
+//            case R.id.tv_wiki_pauling_scale:
+//                uri = "https://en.wikipedia.org/wiki/Electronegativities_of_the_elements_(data_page)";
+//                intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+//                startActivity(intent);
+//                break;
+//
+//            case R.id.img_wiki_table_isotope:
+//                uri = "https://en.wikipedia.org/wiki/Table_of_nuclides_(combined)";
+//                intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+//                startActivity(intent);
+//                break;
 
             case R.id.electron_view:
                 intent = new Intent(this, ConfigElectronActivity.class);
@@ -320,7 +361,15 @@ public class DetailElementActivity extends AppCompatActivity implements View.OnC
                 showDialog(Constraint.FILE_ELECTRON, "Electron");
                 break;
 
+            case R.id.ib_info_electron:
+                showDialog(Constraint.FILE_ELECTRON, "Electron");
+                break;
+
             case R.id.ln_proton:
+                showDialog(Constraint.FILE_PROTON, "Proton");
+                break;
+
+            case R.id.ib_info_proton:
                 showDialog(Constraint.FILE_PROTON, "Proton");
                 break;
 
@@ -328,7 +377,15 @@ public class DetailElementActivity extends AppCompatActivity implements View.OnC
                 showDialog(Constraint.FILE_NEUTRON, "Nơtron");
                 break;
 
+            case R.id.ib_info_neutron:
+                showDialog(Constraint.FILE_NEUTRON, "Nơtron");
+                break;
+
             case R.id.tv_element_category:
+                handleCategory(mStrCategory);
+                break;
+
+            case R.id.ib_info_category:
                 handleCategory(mStrCategory);
                 break;
 
@@ -336,7 +393,15 @@ public class DetailElementActivity extends AppCompatActivity implements View.OnC
                 showDialog(Constraint.FILE_WEIGHT, "Nguyên tử khối");
                 break;
 
+            case R.id.ib_info_weight:
+                showDialog(Constraint.FILE_WEIGHT, "Nguyên tử khối");
+                break;
+
             case R.id.tv_group:
+                showDialog(Constraint.FILE_GROUP, "Nhóm");
+                break;
+
+            case R.id.ib_info_group:
                 showDialog(Constraint.FILE_GROUP, "Nhóm");
                 break;
 
@@ -344,7 +409,15 @@ public class DetailElementActivity extends AppCompatActivity implements View.OnC
                 showDialog(Constraint.FILE_PERIODIC, "Chu kỳ");
                 break;
 
+            case R.id.ib_info_period:
+                showDialog(Constraint.FILE_PERIODIC, "Chu kỳ");
+                break;
+
             case R.id.tv_electronegativity:
+                showDialog(Constraint.FILE_ELECTRONEGATIVITY, "Độ âm điện");
+                break;
+
+            case R.id.ib_info_electronegativity:
                 showDialog(Constraint.FILE_ELECTRONEGATIVITY, "Độ âm điện");
                 break;
 
@@ -352,11 +425,23 @@ public class DetailElementActivity extends AppCompatActivity implements View.OnC
                 showDialog(Constraint.FILE_VALENCE, "Hóa trị");
                 break;
 
+            case R.id.ib_info_valence:
+                showDialog(Constraint.FILE_VALENCE, "Hóa trị");
+                break;
+
             case R.id.tv_class:
                 showDialog(Constraint.FILE_CLASS, "Phân lớp");
                 break;
 
+            case R.id.ib_info_class:
+                showDialog(Constraint.FILE_CLASS, "Phân lớp");
+                break;
+
             case R.id.tv_simplified_configuration:
+                showDialog(Constraint.FILE_CONFIGURATION, "Cấu hình electron");
+                break;
+
+            case R.id.ib_info_configuration:
                 showDialog(Constraint.FILE_CONFIGURATION, "Cấu hình electron");
                 break;
 
@@ -368,11 +453,14 @@ public class DetailElementActivity extends AppCompatActivity implements View.OnC
                 showDialog(Constraint.FILE_ISOTOPE, "Đồng vị");
                 break;
 
+            case R.id.ib_info_isotope:
+                showDialog(Constraint.FILE_ISOTOPE, "Đồng vị");
+                break;
         }
 
     }
 
-    private void showDialog(String value, String name){
+    private void showDialog(String value, String name) {
         dialog = new Dialog(this);
         dialog.setContentView(R.layout.item_dialog_info_element);
         mTvInfoElement = dialog.findViewById(R.id.tv_info_element);
@@ -404,38 +492,39 @@ public class DetailElementActivity extends AppCompatActivity implements View.OnC
         dialog.show();
     }
 
-    private void handleCategory(String category){
-        switch (category){
+    private void handleCategory(String category) {
+        switch (category) {
             case "Kim loại kiềm":
-                showDialog(Constraint.FILE_ALKALI_METAL ,category);
+                showDialog(Constraint.FILE_ALKALI_METAL, category);
                 break;
             case "Kim loại kiềm thổ":
-                showDialog(Constraint.FILE_ALKALINE_EARTH_METAL ,category);
+                showDialog(Constraint.FILE_ALKALINE_EARTH_METAL, category);
                 break;
             case "Kim loại yếu":
-                showDialog(Constraint.FILE_POST_TRANSITION_METAL ,category);
+                showDialog(Constraint.FILE_POST_TRANSITION_METAL, category);
                 break;
             case "Á kim":
-                showDialog(Constraint.FILE_METALLOID ,category);
+                showDialog(Constraint.FILE_METALLOID, category);
                 break;
             case "Kim loại chuyển tiếp":
-                showDialog(Constraint.FILE_TRANSITION_METAL ,category);
+                showDialog(Constraint.FILE_TRANSITION_METAL, category);
                 break;
             case "Phi kim":
-                showDialog(Constraint.FILE_NONMETAL ,category);
+                showDialog(Constraint.FILE_NONMETAL, category);
                 break;
             case "Halogen":
-                showDialog(Constraint.FILE_HALOGEN ,category);
+                showDialog(Constraint.FILE_HALOGEN, category);
                 break;
             case "Khí trơ":
-                showDialog(Constraint.FILE_NOBLE_GAS ,category);
+                showDialog(Constraint.FILE_NOBLE_GAS, category);
                 break;
             case "Họ Lantan":
-                showDialog(Constraint.FILE_LANTHANIDE ,category);
+                showDialog(Constraint.FILE_LANTHANIDE, category);
                 break;
             case "Họ Actini":
-                showDialog(Constraint.FILE_ACTINIDE ,category);
+                showDialog(Constraint.FILE_ACTINIDE, category);
                 break;
         }
     }
+
 }
